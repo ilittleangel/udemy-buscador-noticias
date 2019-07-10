248b9811503c54f74d77dae4baa7303d049596e8
ilittleangel <angel.rojo.perez@gmail.com>
2019-07-10 20:39:10 +0200

    feat: async request to the REST API and Header.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header'
+
 
 class App extends React.Component {
+  state = {
+    noticias: []
+  };
+
+  componentDidMount() {
+    this.consultarNoticias()
+  }
+
+  consultarNoticias = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=business&apiKey=1c9686b7dffb4650b357e47ffd5a19ad`;
+    const response = await fetch(url);
+    const noticias = await response.json();
+    this.setState({
+      noticias: noticias.articles
+    })
+  };
 
   render() {
     return(
-        <h1>Noticias</h1>
+        <Fragment>
+          <Header titulo="Noticias API REST"/>
+        </Fragment>
     )
   }
 
