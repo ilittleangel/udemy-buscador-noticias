1a183df09e1ed8a48a92a073dad5fc44930546dc
ilittleangel <angel.rojo.perez@gmail.com>
2019-07-10 21:45:12 +0200

    feat: show the source.name inside the image

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -5,7 +5,15 @@ const Noticia = ({noticia}) => {
 
     // aplicando destructuring extraemos cada uno de
     // los elementos de la noticia
-    const {urlToImage, author, url, title, description, source} = noticia;
+    const {urlToImage, url, title, description, source} = noticia;
+
+    const imagen = (urlToImage) ?
+        <div className="card-image">
+            <img src={urlToImage} alt={title}/>
+            <span className="card-title">{source.name}</span>
+        </div>
+
+        : null;
 
 
     return (
@@ -23,9 +31,7 @@ const Noticia = ({noticia}) => {
         <div className="col s12 m6 l4">
             <div className="card">
 
-                <div className="card-image">
-                    <img src={urlToImage} alt={title}/>
-                </div>
+                {imagen}
 
                 <div className="card-content">
                     <h3>{title}</h3>
